de9ecf497c977a5622fd6726ba43b391ffdaf45f
aryafirmansyah2 <aryafirmansyah200404@example.com>
2024-06-09 22:52:08 +0700

    test

--- src/app/api/v1/public/role/route.js
+++ src/app/api/v1/public/role/route.js
@@ -1,44 +1,41 @@
-import db from "@/lib/db";
+import db from '@/lib/db';
 
-import { successResponse } from "@/lib/genericResponse";
-import { internalErrorResponse, notFoundResponse } from "@/lib/errorException";
+import { successResponse } from '@/lib/genericResponse';
+import { internalErrorResponse, notFoundResponse } from '@/lib/errorException';
 
-export async function GET(
-    req
-) {
-    try {
-        const url = new URL(req.url);
-        const searchParams = url.searchParams;
+export async function GET(req) {
+  try {
+    const url = new URL(req.url);
+    const searchParams = url.searchParams;
 
-        if (searchParams.get('id')) {
-            const id = searchParams.get('id');
+    if (searchParams.get('id')) {
+      const id = searchParams.get('id');
 
-            const data = await db.role.findUnique({
-                where: {
-                    id: id
-                }
-            });
+      const data = await db.role.findUnique({
+        where: {
+          id: id,
+        },
+      });
 
-            if (!data) {
-                return Response.json(notFoundResponse(), { status: 404 });
-            }
+      if (!data) {
+        return Response.json(notFoundResponse(), { status: 404 });
+      }
 
-            return Response.json(successResponse(data, 1), { status: 200 });
-        }
-
-        let baseQuery = {
-            include: {},
-            orderBy: {
-                created_at: 'desc'
-            }
-        };
-        
-
-        const datas = await db.role.findMany(baseQuery)
-
-        return Response.json(successResponse(datas, datas.length), { status: 200 });
-    } catch (error) {
-        console.log('[ROLE_GET]', error);
-        return Response.json(internalErrorResponse(error), { status: 500 });
+      return Response.json(successResponse(data, 1), { status: 200 });
     }
+
+    let baseQuery = {
+      include: {},
+      orderBy: {
+        created_at: 'desc',
+      },
+    };
+
+    const datas = await db.role.findMany(baseQuery);
+
+    return Response.json(successResponse(datas, datas.length), { status: 200 });
+  } catch (error) {
+    // console.log('[ROLE_GET]', error);
+    return Response.json(internalErrorResponse(error), { status: 500 });
+  }
 }
