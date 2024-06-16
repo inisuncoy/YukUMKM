f3e841c8cfc89372a63cde5055c519b114da74b3
Faiz Elfahad Kurniawan <74404911+siunlucky@users.noreply.github.com>
2024-06-16 22:44:02 +0700

    Item - CRUD System

---
+++ src/app/api/v1/public/itemCategory/route.js
@@ -0,0 +1,50 @@
+import db from "@/lib/db";
+
+import { successResponse } from "@/lib/genericResponse";
+import { internalErrorResponse, notFoundResponse } from "@/lib/errorException";
+import logger from "@/services/logger";
+
+
+export async function GET(
+    req
+) {
+    try {
+        const url = new URL(req.url);
+        const searchParams = url.searchParams;
+
+        if (searchParams.get('id')) {
+            const id = searchParams.get('id');
+
+            const data = await db.itemCategory.findUnique({
+                where: {
+                    id: id
+                }
+            });
+
+            if (!data) {
+                return Response.json(notFoundResponse(), { status: 404 });
+            }
+
+            return Response.json(successResponse(data, 1), { status: 200 });
+        }
+
+        let baseQuery = {
+            include: {},
+            orderBy: {
+                created_at: 'desc'
+            }
+        };
+        
+
+        const datas = await db.itemCategory.findMany(baseQuery)
+
+        logger.info(req);
+        return Response.json(successResponse(datas, datas.length), { status: 200 });
+    } catch (error) {
+        console.log('[ITEM_CATEGORY_GET]', error);
+        logger.error(req, {
+            stack: error,
+        });
+        return Response.json(internalErrorResponse(error), { status: 500 });
+    }
+}
