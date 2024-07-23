9a3758e7023de6a4ca9e180de143e84cf607761d
Faiz Elfahad <74404911+siunlucky@users.noreply.github.com>
2024-07-23 23:11:25 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-         {
-           key: 'Content-Security-Policy',
-           value: "upgrade-insecure-requests"
-         }
-        ],
-      },
-    ]
-  },
+  //  async headers() {
+  //   return [
+  //     {
+  //       source: '/:path*',
+  //       headers: [
+  //        {
+  //          key: 'Content-Security-Policy',
+  //          value: "upgrade-insecure-requests"
+  //        }
+  //       ],
+  //     },
+  //   ]
+  // },
   images: {
     remotePatterns: [
       {
